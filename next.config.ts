572832fff9366b11ed7a01d2c9d665a5bb8c69f6
Weezzt <william.vesterberg@lerniastudent.se>
2025-09-15 14:12:11 +0200

    fixed config

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,17 @@ const nextConfig: NextConfig = {
   output: "standalone",
   images: {
     remotePatterns: [
+      // Virtual-hosted–style S3 URL
       {
         protocol: "https",
-        hostname: "allmskog-ac.s3.eu-north-1.amazonaws.com", // replace this
-        pathname: "**",
+        hostname: "allmskog-ac.s3.eu-north-1.amazonaws.com",
+        pathname: "/**",
+      },
+      // Path-style S3 URL (covers any links like s3.amazonaws.com/<bucket>/...)
+      {
+        protocol: "https",
+        hostname: "s3.eu-north-1.amazonaws.com",
+        pathname: "/allmskog-ac/**",
       },
     ],
   },
